1b468a810e2cff72cbc05a4c2b7c895d4b2f38bc
Samrat Mitra <samratmitra338@gmail.com>
2021-05-22 17:55:48 +0530

    Here I have implemented the logic for Reversing a String, same goes for Array as well.
    Both Iterative and Recursive Solutions in Java are provided here.

---
+++ Array/1. Reverse Array Or String/Solution.java
@@ -0,0 +1,46 @@
+// Iterative Approach
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        //Converting the String to character array
+        char[] stringToCharArray = str.toCharArray();
+        int start = 0, end = str.length() - 1;
+        while(start < end){
+            char temp = stringToCharArray[start];
+            stringToCharArray[start] = stringToCharArray[end];
+            stringToCharArray[end] = temp;
+            start++;
+            end--;
+        }
+        //Converting the Character Array to String again
+        return String.valueOf(stringToCharArray);
+    }
+}
+
+//Recursive Approach
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        int start = 0, end = str.length() - 1;
+        String strAns = reverseWordRec(str, start, end);
+        return strAns;
+    }
+    public static String reverseWordRec(String str, int start, int end){
+        //Converting the String to character array
+        char[] stringToCharArray = str.toCharArray();
+        if(start >= end){
+            //Converting the Character Array to String again
+            return String.valueOf(stringToCharArray);
+        }
+        char temp = stringToCharArray[start];
+        stringToCharArray[start] = stringToCharArray[end];
+        stringToCharArray[end] = temp;
+        return reverseWordRec(String.valueOf(stringToCharArray), start+1, end-1);
+    }
+}
