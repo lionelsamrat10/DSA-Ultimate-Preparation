5b4a00adfa1082faee2551700147be6ed7d88365
Samrat Mitra <samratmitra338@gmail.com>
2021-05-23 15:40:00 +0530

    Create Solution.java

---
+++ Binary Search(VVVI) Related Problems/Guess Number Higher or Lower/Solution.java
@@ -0,0 +1,26 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+// Link to question: https://leetcode.com/explore/learn/card/binary-search/125/template-i/951/
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+}
