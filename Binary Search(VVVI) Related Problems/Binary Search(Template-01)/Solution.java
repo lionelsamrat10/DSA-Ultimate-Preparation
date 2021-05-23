b72b26427f941c9a99739c68ed8f76f5770d0898
Samrat Mitra <samratmitra338@gmail.com>
2021-05-23 16:16:36 +0530

    Update Solution.java

--- Binary Search(VVVI) Related Problems/Binary Search(Template-01)/Solution.java
+++ Binary Search(VVVI) Related Problems/Binary Search(Template-01)/Solution.java
@@ -14,3 +14,8 @@ int binarySearch(int[] nums, int target){
   // End Condition: left > right
   return -1;
 }
+
+
+// Most basic and elementary form of Binary Search
+// Search Condition can be determined without comparing to the element's neighbors (or use specific elements around it)
+// No post-processing required because at each step, you are checking to see if the element has been found. If you reach the end, then you know the element is not found
